843e7c3fd595045c1d88140499de0ad138cb2c5d
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-03 20:07:20 +0100

    update tailwind config file by adding container styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ export default {
       mono: ["Roboto Mono", "sans"],
       sans: ["Roboto", "sans"],
     },
+    container: {
+      center: "true",
+      padding: "1rem",
+      screens: {
+        sm: "100%",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
   },
   plugins: [],
 };
