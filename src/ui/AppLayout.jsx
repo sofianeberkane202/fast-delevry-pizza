0d1c16215ae0496c10d7855acbc8965312554eb6
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-03 16:19:50 +0100

    finish building and styling loading component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "./NavBar";
 import TotalPriceAndQuantity from "./TotalPriceAndQuantity";
+import Loader from "./Loader";
 function AppLayout() {
+  const { state } = useNavigation();
+  console.log(state);
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] bg-stone-100">
+    <div className={`relative grid h-screen grid-rows-[auto_1fr_auto] bg-stone-100`}>
       <NavBar />
-
+      <Loader />
       <div className="overflow-y-auto">
         <Outlet />
       </div>
