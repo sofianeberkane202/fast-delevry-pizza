2b0c08a8ceeb1eca54b914f234b0cd1ae3702b87
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-04 12:27:21 +0100

    update padding style for small screens

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,18 +3,18 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 function Button({ type, style, to, children }) {
   const base = `rounded-full bg-yellow-400
-  text-base font-semibold uppercase text-stone-700 
+  text-sm sm:text-base font-semibold uppercase text-stone-700 
   transition-colors duration-300 hover:bg-yellow-500`;
 
   const secondaryBase = `rounded-full text-base font-semibold uppercase
   bg-stone-50 text-stone-400 transition-colors duration-300
-  border-2 border-stone-300 px-6 py-2.5
+  border-2 border-stone-300 px-6 py-2
   hover:bg-stone-200 hover:text-stone-700`;
 
   const linkBase = "font-mono font-medium text-blue-500/80";
 
   const buttonStyle = {
-    primary: `${base} px-6 py-3`,
+    primary: `${base} px-4 py-2.5 sm:px-6 sm:py-3`,
     small: `${base} ${style}`,
     link: `${linkBase} ${style}`,
     secondary: `${secondaryBase}`,
