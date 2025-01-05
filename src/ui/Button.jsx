20280322e04f715dbac95152e1f4f82f92bf4813
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-05 10:10:33 +0100

    adjust responsivity for screens under 450px

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,22 +2,22 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 function Button({ type, style, to, children }) {
-  const base = `sm:rounded-full bg-yellow-400
-  text-sm sm:text-base font-semibold uppercase text-stone-700 
+  const base = `rounded-full bg-yellow-400 text-[12px]
+  sm:text-sm tracking-wider font-semibold uppercase text-stone-700 
   transition-colors duration-300 hover:bg-yellow-500`;
 
-  const secondaryBase = `rounded-full text-sm sm:text-base font-semibold uppercase
-  bg-stone-50 text-stone-400 transition-colors duration-300
+  const secondaryBase = `rounded-full sm:text-sm text-[12px] 
+  font-semibold uppercase bg-stone-50 text-stone-400 transition-colors duration-300
   border-2 border-stone-300 px-6 py-2
   hover:bg-stone-200 hover:text-stone-700`;
 
   const linkBase = "font-mono font-medium text-blue-500/80";
 
   const buttonStyle = {
-    primary: `${base} px-4 py-2.5 sm:px-6 sm:py-3`,
+    primary: `${base} px-3 py-2.5 sm:px-6 sm:py-3`,
     small: `${base} ${style}`,
     link: `${linkBase} ${style}`,
-    secondary: `${secondaryBase}`,
+    secondary: `${secondaryBase} ${style}`,
   };
 
   if (to)
