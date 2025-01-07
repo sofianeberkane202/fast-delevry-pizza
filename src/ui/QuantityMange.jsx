0959f317cc58d7da6caaaf537a2471f84b2d1b2b
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 13:13:24 +0100

    refactor(cart): add quantity handlers and pass them as props to Buttons component
    Created handleDecreaseQuantity and handleIncreaseQuantity functions in QuantityManage. These functions dispatch decreaseQuantity and increaseQuantity actions from reducerCartSlice. Passed the handlers as props to the Buttons component for improved functionality and modularity.

--- src/ui/QuantityMange.jsx
+++ src/ui/QuantityMange.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useDispatch } from "react-redux";
 import Button from "../ui/Button";
 import PropTypes from "prop-types";
-function QuantityMange({ style, buttonStyle, quantity }) {
-  // eslint-disable-next-line no-unused-vars
+import { decreaseQuantity, increaseQuantity } from "../features/Cart/reducerCartSlice";
+
+function QuantityMange({ style, buttonStyle, quantity, pizzaId }) {
+  const dispatch = useDispatch();
   const baseButton = "xs:text-sm flex items-center justify-center";
+
+  function handleDecreaseQuantity() {
+    dispatch(decreaseQuantity(pizzaId));
+  }
+
+  function handleIncreaseQuantity() {
+    dispatch(increaseQuantity(pizzaId));
+  }
 
   return (
     <div className={`flex flex-row items-center sm:gap-2 ${style}`}>
-      <Button type={"small"} style={`${baseButton} ${buttonStyle}`}>
+      <Button handleState={handleDecreaseQuantity} type={"small"} style={`${baseButton} ${buttonStyle}`}>
         -
       </Button>
       <p className="px-2 text-[12px] font-semibold xs:text-sm sm:bg-transparent">{quantity}</p>
-      <Button type={"small"} style={`${baseButton} ${buttonStyle}`}>
+      <Button handleState={handleIncreaseQuantity} type={"small"} style={`${baseButton} ${buttonStyle}`}>
         +
       </Button>
     </div>
@@ -24,4 +35,5 @@ QuantityMange.propTypes = {
   style: PropTypes.string,
   buttonStyle: PropTypes.string,
   quantity: PropTypes.number,
+  pizzaId: PropTypes.number,
 };
