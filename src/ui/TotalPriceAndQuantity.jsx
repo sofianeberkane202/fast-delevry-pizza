249a662bbbc44814b495deed6da0479f41fa42e4
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 21:26:12 +0100

    feat: consume totalPrice and quantity functions in TotalPriceAndQuantity component
    Used `getNumberOfPizzasAdded` and `getBillOfTotalPrice` from reducerCartSlice to retrieve the current total price and quantity. Implemented these in the TotalPriceAndQuantity component to display accurate cart information.

--- src/ui/TotalPriceAndQuantity.jsx
+++ src/ui/TotalPriceAndQuantity.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { getBillOfTotalPrice, getNumberOfPizzasAdded } from "../features/Cart/reducerCartSlice";
 
 /* eslint-disable react/react-in-jsx-scope */
 function TotalPriceAndQuantity() {
+  const numberOfPizzas = useSelector((state) => getNumberOfPizzasAdded(state));
+  const bill = useSelector((state) => getBillOfTotalPrice(state));
   return (
     <div className="flex items-center justify-between bg-stone-800 px-6 py-4 font-mono font-medium uppercase text-stone-100">
       <div className="flex gap-4 text-sm sm:text-base">
-        <p>1 pizzas</p>
-        <p>$16.00</p>
+        <p>{numberOfPizzas} pizzas</p>
+        <p>${Number(bill).toFixed(2)}</p>
       </div>
       <Link to={"/cart"} className="text-sm sm:text-base">
         open cart &rarr;
