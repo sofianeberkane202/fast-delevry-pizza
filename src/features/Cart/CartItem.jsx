d1eca882b4d01d2783c65e447b1d56dfc4529e2b
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 15:47:33 +0100

    feat(cart): handle delete button for small screens in CartItem component
    Updated the CartItem component to improve the delete button's usability on small screens. Adjusted styles and layout to ensure proper functionality and responsive design.

--- src/features/Cart/CartItem.jsx
+++ src/features/Cart/CartItem.jsx
@@ -36,17 +36,23 @@ function CartItem({ item }) {
           Delete
         </Button>
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="#d97706"
-          className="bi bi-trash h-5 w-5 cursor-pointer sm:hidden"
-          viewBox="0 0 16 16"
+        <Button
+          type={"small"}
+          style={"h-5 w-5 bg-transparent px-0 py-0 hover:bg-transparent"}
+          handleState={handleDeleteCartItem}
         >
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="#d97706"
+            className="bi bi-trash h-full w-full"
+            viewBox="0 0 16 16"
+          >
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+          </svg>
+        </Button>
       </div>
     </li>
   );
