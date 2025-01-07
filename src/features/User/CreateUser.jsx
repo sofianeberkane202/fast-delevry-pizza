58cb43529c68e97a0df9c00404e57a0f895d5fcd
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 20:47:12 +0100

    feat: create CreateUser component for managing username
    Added the `CreateUser` component to handle username creation. The component includes a form with an input field and a button. It dispatches the `createUser` action from the `reducerUserSlice` to update the user state in the Redux store.

---
+++ src/features/User/CreateUser.jsx
@@ -0,0 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { createUser } from "./reducerUserSlice";
+import { useState } from "react";
+
+/* eslint-disable react/react-in-jsx-scope */
+function CreateUser() {
+  const [name, setName] = useState("");
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(createUser(name));
+
+    navigate("/menu");
+  }
+  return (
+    <form className="flex flex-col items-center gap-4" onSubmit={(e) => handleSubmit(e)}>
+      <input
+        type="text"
+        placeholder="Your full name"
+        className="w-72 rounded-full px-4 py-3 text-sm"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <Button type={"primary"}>start ordering</Button>
+    </form>
+  );
+}
+
+export default CreateUser;
