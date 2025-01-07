d84a7e707666cbc98e29d21c38fc7db95ef1e764
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 20:48:48 +0100

    refactor: replace form in HomePage with CreateUser component to handle username
    Replaced the existing form in the `HomePage` component with the `CreateUser` component. This change delegates the responsibility of handling username creation to the reusable `CreateUser` component, improving modularity and code reuse.

--- src/ui/HomePage.jsx
+++ src/ui/HomePage.jsx
@@ -1,17 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { useNavigate } from "react-router-dom";
-import Button from "./Button";
+import CreateUser from "../features/User/CreateUser";
+
 function HomePage() {
   return <Content />;
 }
 
 function Content() {
-  const navigate = useNavigate();
-  function handleSubmit(e) {
-    e.preventDefault();
-    navigate("/menu");
-  }
   return (
     <main className="relative text-center">
       <div className="absolute left-1/2 top-10 w-[80%] -translate-x-1/2 sm:top-16">
@@ -23,11 +18,7 @@ function Content() {
         <p className="mb-4 text-sm sm:text-base sm:tracking-widest">
           👋 Welcome! Please start by telling us your name:
         </p>
-        <form className="flex flex-col items-center gap-4" onSubmit={(e) => handleSubmit(e)}>
-          <input type="text" placeholder="Your full name" className="w-72 rounded-full px-4 py-3 text-sm" />
-
-          <Button type={"primary"}>start ordering</Button>
-        </form>
+        <CreateUser />
       </div>
     </main>
   );
