b272deabfcbb081f83b3a6e231a4f794d3483a57
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-07 15:44:06 +0100

    feat(CartPage): add clear cart functionality and conditional rendering in CartPage
    Added a new function in the CartPage component to clear the cart when the "Clear" button is clicked. This function dispatches the clearCart action from reducerCartSlice. Also, integrated the CartEmpty component to handle conditional rendering between CartEmpty and CartItems based on the cart state.

--- src/features/Cart/CartPage.jsx
+++ src/features/Cart/CartPage.jsx
@@ -1,29 +1,45 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
-import { getCart } from "./reducerCartSlice";
+import { clearCart, getCart } from "./reducerCartSlice";
+import CartEmpty from "./CartEmpty";
 
 function CartPage() {
   const cart = useSelector((state) => getCart(state));
+
+  const dispatch = useDispatch();
+
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
   return (
     <div className="mt-4">
       <Button to={"/menu"} type={"link"} style={"text-sm"}>
         &larr; Back to menu
       </Button>
-      <h2 className="mt-10 font-mono text-lg font-semibold tracking-wide">You cart, %Name%</h2>
-      <ul className="divide-y-2 divide-stone-200">
-        {cart.map((item) => (
-          <CartItem item={item} key={item.pizzaId} />
-        ))}
-      </ul>
-      <div className="mt-4 space-x-3">
-        <Button to={"/order/new"} type={"primary"}>
-          Order pizzas
-        </Button>
-        <Button type={"secondary"}>clear cart</Button>
-      </div>
+
+      {cart.length === 0 && <CartEmpty />}
+
+      {cart.length > 0 && (
+        <>
+          <h2 className="mt-10 font-mono text-lg font-semibold tracking-wide">You cart, %Name%</h2>
+          <ul className="divide-y-2 divide-stone-200">
+            {cart.map((item) => (
+              <CartItem item={item} key={item.pizzaId} />
+            ))}
+          </ul>
+          <div className="mt-4 space-x-3">
+            <Button to={"/order/new"} type={"primary"}>
+              Order pizzas
+            </Button>
+            <Button type={"secondary"} handleState={handleClearCart}>
+              clear cart
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
