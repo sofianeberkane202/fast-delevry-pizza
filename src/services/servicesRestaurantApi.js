c2b9855ddc5a8bce279010760bd1e30c2ad7b60c
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-08 13:49:59 +0100

    Add postUser async function in servicesRestaurantApi to send user data via POST request, with error handling and response parsing

--- src/services/servicesRestaurantApi.js
+++ src/services/servicesRestaurantApi.js
@@ -1,8 +1,28 @@
-const url = "http://localhost:8000/data";
+const url = "http://localhost:8000";
 
 export async function getPizzas() {
-  const response = await fetch(url);
+  const response = await fetch(`${url}/data`);
   if (!response.ok) throw new Error("Faild to fetch data!");
   const data = response.json();
   return data;
+}
+
+export async function postUser(userData) {
+  try {
+    const response = await fetch(`${url}/users`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) throw new Error(`Faild to submit user data: ${response.statusText}`);
+
+    const result = await response.json();
+
+    return { success: true, message: "Customer data submitted successfully!", data: result };
+  } catch (error) {
+    console.error("Error submitting customer data:", error);
+
+    return { success: false, message: "Faild to submit customer data. Please try again later." };
+  }
 }
