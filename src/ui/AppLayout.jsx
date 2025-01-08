434d763082a7cb5d0d29627b108a75f48ef2564c
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-08 11:28:00 +0100

    feat: consume name from reducerUserSlice to control TotalPriceQuantity display
    Updated the logic to fetch the user's name from `reducerUserSlice` and conditionally display the `TotalPriceQuantity` component based on whether a name exists in the store.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,8 +4,11 @@ import { Outlet, useNavigation } from "react-router-dom";
 import NavBar from "./NavBar";
 import TotalPriceAndQuantity from "./TotalPriceAndQuantity";
 import Loader from "./Loader";
+import { useSelector } from "react-redux";
+import { getName } from "../features/User/reducerUserSlice";
 function AppLayout() {
   const { state } = useNavigation();
+  const name = useSelector((state) => getName(state));
 
   return (
     <div className="relative grid h-screen grid-rows-[auto_1fr_auto] bg-stone-100">
@@ -13,7 +16,7 @@ function AppLayout() {
       {state === "loading" && <Loader />}
       <div className="container overflow-y-auto">{state === "idle" && <Outlet />}</div>
 
-      <TotalPriceAndQuantity />
+      {name && <TotalPriceAndQuantity />}
     </div>
   );
 }
