fde641dfd0dcf143606ba81d56ec44cb16d5f525
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-09 11:37:10 +0100

    add new controling CreateUser component JSX by consuming userName data from reducerUserSlice file

--- src/features/User/CreateUser.jsx
+++ src/features/User/CreateUser.jsx
@@ -1,16 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../ui/Button";
-import { useDispatch } from "react-redux";
-import { createUser } from "./reducerUserSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { createUser, getName } from "./reducerUserSlice";
 import { useState } from "react";
 
 /* eslint-disable react/react-in-jsx-scope */
 function CreateUser() {
+  const userName = useSelector((state) => getName(state));
   const [name, setName] = useState("");
 
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
     dispatch(createUser(name));
@@ -19,15 +21,20 @@ function CreateUser() {
   }
   return (
     <form className="flex flex-col items-center gap-4" onSubmit={(e) => handleSubmit(e)}>
-      <input
-        type="text"
-        placeholder="Your full name"
-        className="w-72 rounded-full px-4 py-3 text-sm outline-none transition-all duration-200 focus:ring focus:ring-yellow-400"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
-      <Button type={"primary"}>start ordering</Button>
+      {!userName ? (
+        <>
+          <input
+            type="text"
+            placeholder="Your full name"
+            className="w-72 rounded-full px-4 py-3 text-sm outline-none transition-all duration-200 focus:ring focus:ring-yellow-400"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button type={"primary"}>start ordering</Button>
+        </>
+      ) : (
+        <Button type={"primary"}>continue ordering, {userName}</Button>
+      )}
     </form>
   );
 }
