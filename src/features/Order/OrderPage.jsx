e295720e2c922e820203fe4c70621579d2ed0128
sofiane berkane <sofianeberkane202@gmail.com>
2025-01-09 11:28:50 +0100

    add new featuers into OrderPage component: -  create new async loader function for fetching order data by using id gotten from url by using params props -  consume data gotten in loader function by using useLoaderData  and structure them in a new order object -  update some styling for risponsivity in JSX

--- src/features/Order/OrderPage.jsx
+++ src/features/Order/OrderPage.jsx
@@ -1,45 +1,57 @@
 /* eslint-disable react/react-in-jsx-scope */
 import PropTypes from "prop-types";
+import { getOrder } from "../../services/servicesRestaurantApi";
+import { useLoaderData } from "react-router-dom";
 
-const fakeCart = [
-  {
-    pizzaId: 1,
-    name: "Margherita Margherita",
-    quantity: 1,
-    unitPrice: 12,
-    totalPrice: 12,
-    ingredients: ["tomato", "mozzarella", "basil"],
-  },
-  {
-    pizzaId: 2,
-    name: "Romana",
-    quantity: 3,
-    unitPrice: 15,
-    totalPrice: 45,
-    ingredients: ["tomato", "mozzarella", "ham", "mushrooms", "artichoke"],
-  },
-  {
-    pizzaId: 3,
-    name: "Prosciutto e Rucola",
-    quantity: 4,
-    unitPrice: 16,
-    totalPrice: 64,
-    ingredients: ["tomato", "mozzarella", "ham", "mushrooms", "artichoke"],
-  },
-];
+// const fakeCart = [
+//   {
+//     pizzaId: 1,
+//     name: "Margherita Margherita",
+//     quantity: 1,
+//     unitPrice: 12,
+//     totalPrice: 12,
+//     ingredients: ["tomato", "mozzarella", "basil"],
+//   },
+//   {
+//     pizzaId: 2,
+//     name: "Romana",
+//     quantity: 3,
+//     unitPrice: 15,
+//     totalPrice: 45,
+//     ingredients: ["tomato", "mozzarella", "ham", "mushrooms", "artichoke"],
+//   },
+//   {
+//     pizzaId: 3,
+//     name: "Prosciutto e Rucola",
+//     quantity: 4,
+//     unitPrice: 16,
+//     totalPrice: 64,
+//     ingredients: ["tomato", "mozzarella", "ham", "mushrooms", "artichoke"],
+//   },
+// ];
 
-const fakeOrder = {
-  id: "defr145dd",
-  firstName: "sofiane berkane",
-  phoneNumber: "072458964",
-  address: "Tazmalt Bejaia Algerie",
-  cart: fakeCart,
-  priority: true,
-};
+// const fakeOrder = {
+//   id: "defr145dd",
+//   firstName: "sofiane berkane",
+//   phoneNumber: "072458964",
+//   address: "Tazmalt Bejaia Algerie",
+//   cart: fakeCart,
+//   priority: true,
+// };
+
 function OrderPage() {
-  const order = fakeOrder;
+  // const order = fakeOrder;
+
+  const data = useLoaderData();
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    totalPrice: +data.totalPrice,
+    priorityPrice: +data.priorityPrice,
+  };
   return (
-    <div className="mt-8 grid gap-6 sm:gap-8">
+    <div className="mb-2 mt-4 grid gap-6 xs:mt-8 sm:gap-8">
       <div className="flex flex-col-reverse items-center justify-between gap-4 sm:flex-row sm:gap-0">
         <h2 className="text-xl font-semibold">Order #{order.id} status</h2>
         <p className="flex items-center gap-2">
@@ -59,16 +71,20 @@ function OrderPage() {
         <p className="text-xs text-stone-500">(Estimated delivery: Jan 8, 04:58 PM)</p>
       </div>
 
-      <ul className="divide-y-2 divide-stone-200">
+      <ul className="h-44 divide-y-2 divide-stone-200 overflow-y-auto px-2">
         {order.cart.map((item) => (
-          <CartItem key={item.id} item={item} />
+          <CartItem key={item.pizzaId} item={item} />
         ))}
       </ul>
 
       <div className="grid gap-2 bg-stone-200 px-6 py-4">
-        <p className="text-sm font-semibold text-stone-500">Price pizza: &euro;27.00</p>
-        <p className="text-sm font-semibold text-stone-500">Price priority: &euro;5.00</p>
-        <p className="text-sm font-semibold">To pay delivery: &euro;27.00</p>
+        <p className="text-sm font-semibold text-stone-500">
+          Price pizza: &euro;{Number(order.totalPrice - order.priorityPrice).toFixed(2)}
+        </p>
+        <p className="text-sm font-semibold text-stone-500">
+          Price priority: &euro;{Number(order.priorityPrice).toFixed(2)}
+        </p>
+        <p className="text-sm font-semibold">To pay delivery: &euro;{Number(order.totalPrice).toFixed(2)}</p>
       </div>
     </div>
   );
@@ -89,6 +105,16 @@ function CartItem({ item }) {
       <p className="text-sm font-bold">&euro;{Number(unitPrice).toFixed(2)}</p>
     </li>
   );
+}
+
+export async function loader({ params }) {
+  const { id } = params;
+  try {
+    const order = await getOrder(id);
+    return order;
+  } catch (error) {
+    return error;
+  }
 }
 
 export default OrderPage;
